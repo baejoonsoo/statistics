7ff8494e5ab6ae846eaa9aa07f67c22431d51189
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-08-29 23:44:49 +0900

    c++ 버전

---
+++ index.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+
+
+
+using namespace std;
+
+
+int main(){
+  srand((unsigned int)time(NULL));
+
+  unsigned long long int statistics[2]={0,0};
+  unsigned long long int number;
+
+  cout<<"몇개의 자료를 사용하시겠습니까? : ";
+  cin>>number;
+
+  for(unsigned long long int i=0;i<number;i++){
+    statistics[rand()%2]++;
+  }
+
+  cout<<statistics[0]<<" : "<<statistics[1]<<endl;
+}
